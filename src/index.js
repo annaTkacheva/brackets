4cbecf7d2b7de9e58cea851d847a7183eb9bed0d
Anna Tkacheva <58954463+annaTkacheva@users.noreply.github.com>
2020-10-25 18:19:00 +0300

    fix: edit index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 1) {
+      return false;
+  }
+  var arr = new Array();
+  for (var i = 0; i <= str.length - 1; i++) {
+      var find = false;
+      for (var j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] == bracketsConfig[j][0]) {
+              find = true;
+              if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                  if (arr[arr.length - 1] == bracketsConfig[j][0]) {
+                      arr.pop();
+                      break;
+                  }
+              }
+              arr.push(bracketsConfig[j][1]);
+              break;
+          }
+      }
+      if (!find) {
+          if (arr.length == 0 || str[i] != arr[arr.length - 1]) {
+              return false;
+          }
+          arr.pop();
+      }
+  }
+  return arr.length == 0 ? true : false;
 }
